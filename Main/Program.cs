d3a174b2b6869e69f9cc248a92ed86b0a0b9f5c9
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2024-05-06 02:00:37 +0700

    Library Aljabar Implemented

---
+++ Main/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AljabarLibraries;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Contoh penggunaan fungsi AkarPersamaanKuadrat dari kelas Library
+        double[] akar = AljabarLibraries.Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine("AkarPersamaanKuadrat({1, -3, -10}): " + string.Join(", ", akar));
+
+        // Contoh penggunaan fungsi HasilKuadrat dari kelas Library
+        double[] hasil = AljabarLibraries.Aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine("HasilKuadrat({2, -3}): " + string.Join(", ", hasil));
+    }
+}
